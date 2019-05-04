674723d18ff859bebea002d0af1a1b7583e0ea3d
Zacharias Mitzelos <accounts@mitzelos.com>
2019-05-04 13:11:56 +0300

    Add Procfile, minor edits

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
-  }
-  
+}
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require("./routes/routes.js");
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
 const path = require('path');
 
 const app = express();
@@ -15,11 +13,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(cookieParser());
-
 app.use(routes);
 
 app.use((req, res, next) => {
